eda11419c273ce8dca22da4cb57150cc90ee042d
Haris Osmanagic <haris@meroxa.io>
2023-06-26 18:55:54 +0200

    updates

--- app.go
+++ app.go
@@ -21,44 +21,12 @@ var _ turbine.App = (*App)(nil)
 type App struct{}
 
 func (a App) Run(v turbine.Turbine) error {
-	// To configure your data stores as resources on the Meroxa Platform
-	// use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider.
-	// For more details refer to: https://docs.meroxa.com/
-	//
-	// Identify an upstream data store for your data app
-	// with the `Resources` function
-	// Replace `source_name` with the resource name the
-	// data store was configured with on Meroxa.
-
 	source, err := v.Resources("pg_db_again")
 	if err != nil {
 		return err
 	}
 
-	// Specify which upstream records to pull
-	// with the `Records` function
-	// Replace `collection_name` with a table, collection,
-	// or bucket name in your data store.
-	// If a configuration is needed for your source,
-	// you can pass it as a second argument to the `Records` function. For example:
-	//
-	// source.Records("collection_name", turbine.ConnectionOptions{turbine.ResourceConfig{Field: "incrementing.field.name", Value:"id"}})
-
 	rr, err := source.Records("products", nil)
-	if err != nil {
-		return err
-	}
-
-	// Specify what code to execute against upstream records
-	// with the `Process` function
-	// Replace `Anonymize` with the name of your function code.
-
-	// Identify a downstream data store for your data app
-	// with the `Resources` function
-	// Replace `destination_name` with the resource name the
-	// data store was configured with on Meroxa.
-
-	res, err := v.Process(rr, NoOp{})
 	if err != nil {
 		return err
 	}
@@ -68,19 +36,7 @@ func (a App) Run(v turbine.Turbine) error {
 		return err
 	}
 
-	// Specify where to write records downstream
-	// using the `Write` function
-	// Replace `collection_archive` with a table, collection,
-	// or bucket name in your data store.
-	// If a configuration is needed, you can also use i.e.
-	//
-	// dest.WriteWithConfig(
-	//  res,
-	//  "my-archive",
-	//  turbine.ConnectionOptions{turbine.ResourceConfig{Field: "buffer.flush.time", Value: "10"}}
-	// )
-
-	err = dest.Write(res, "products_enriched")
+	err = dest.Write(rr, "products_enriched")
 	if err != nil {
 		return err
 	}
